d70fd5b7c70c50341eef1a02e0f50842d105e2d8
AfriCoffee <belannawalker2015@gmail.com>
2016-07-07 13:54:58 -0700

    Signed-off-by: AfriCoffee <belannawalker2015@gmail.com>

---
+++ Lab/Crap_V2/main.cpp
@@ -0,0 +1,84 @@
+/* 
+ * File:   main.cpp
+ * Author: B'Elanna Walker
+ *
+ * Created on August 20, 2014, 2:45 PM
+ //Purpose: First Program
+ */
+
+//System Libraries
+#include <iostream> //Imput/Output Stream Library
+#include <ctime>    //Time for random seed
+#include <cstdlib>  //random number seed
+#include <iomanip>  //formating
+#include <cmath>    //Math Library
+#include <fstream>  //File I/O
+#include <string>   //String Object
+
+using namespace std; //Iostream uses the standard namespace
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+/*
+ * 
+ */
+int main(int argc, char** argv) 
+{
+    //Set Random Number Seed
+    srand(static_cast<unsigned int>(time(0)));
+    
+    //Declare Variables Here, NO DOUBLES
+    int nGames;
+    int nWins=0;
+    int nLose=0;
+    
+    //Input Data
+    do{
+    cout<<"The Game of Craps"<<endl;
+    cout<<"How many games to play? (Must be positive)"<<endl;
+    cin>>nGames;
+    }while (nGames<=0);
+    //Process Data
+    for(int game=1;game<=nGames;game++){
+        //Throw a pair of dice
+        char die1=rand()%6+1;
+        char die2=rand()%6+1;
+        char sum=die1+die2;
+        //Determine Win or Loss
+        if (sum==7||sum==11)nWins++;
+        else if (sum==2||sum==3||sum==12)nLose++;
+        else{
+            //When to Roll again
+            bool rollAgn=true;
+            do{
+                //Throw another set of dice
+                die1=rand()%6+1;
+                die2=rand()%6+1;
+                char sumAgn=die1+die2;
+                if(sum==sumAgn){
+                    nWins++;
+                    rollAgn=false;}
+                else if(sumAgn==7){
+                    nLose++;
+                    rollAgn=false;}
+                else rollAgn=true;
+            }while(rollAgn);
+    }
+    
+    //Output Data
+        cout<<"Number of Games = "<<nGames<<endl;
+        cout<<"Number of Wins = "<<nWins<<endl;
+        cout<<"Number of Losses ="<<nLose<<endl;
+    
+    //Exit Stage Right!
+    return 0;
+    }
+}
+
+
+
